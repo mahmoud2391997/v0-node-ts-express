5b0c55a0ef4908a569442025b51d7dfce832576a
mahmoudmelsaid1 <mahmoudmelsaid1@gmail.com>
2025-11-22 21:43:20 +0000

    adsasdads

--- .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++ .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
@@ -2,7 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/[root-of-the-server]__51d697a8._.css",
-  "static/chunks/node_modules_b600f83d._.js",
+  "static/chunks/node_modules_b7bc1f12._.js",
   "static/chunks/src_components_Sidebar_tsx_ca983420._.js"
 ],
     source: "dynamic"
